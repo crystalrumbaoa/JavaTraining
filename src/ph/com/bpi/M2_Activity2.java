f185b3711b5a853bde2c239e393b4381f16286e5
Crystal Rumbaoa <rumbaoacrystal0528@gmail.com>
2025-12-05 14:24:08 +0800

    Add files via upload

---
+++ src/ph/com/bpi/M2_Activity2.java
@@ -0,0 +1,13 @@
+package ph.com.bpi;
+
+public class M2_Activity2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WhiteBoard wb = new WhiteBoard();
+		int sum = wb.addNumbers(5,3);
+		System.out.println("The total is: " + String.valueOf(sum));
+	}
+
+}
